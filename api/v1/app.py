208923e55b04631c7bb23a7061949df398d09286
husham35 <husham35@gmail.com>
2024-05-28 21:24:03 +0000

    fix for api/v1/views/__init__.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,12 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 from api.v1.views import app_views
-
 from models import storage
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, origins="0.0.0.0")
+CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
