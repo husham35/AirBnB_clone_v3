162310402abc3dfdcce64887871289b259f9acfe
husham35 <husham35@gmail.com>
2024-05-28 21:35:03 +0000

    fix for api/v1/views/__init__.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Module for api """
-from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify
 from flask_cors import CORS
-
-from api.v1.views import app_views
 from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
